ef95473c787cc7c28ec56dfa28b51858865caf18
becoding96 <becoding96@gmail.com>
2022-07-23 21:29:18 +0900

    2738

---
+++ bronze/2738_행렬_덧셈.py
@@ -0,0 +1,10 @@
+# 220723
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+b = [list(map(int, input().split())) for _ in range(n)]
+c = [[(a[i][j] + b[i][j]) for j in range(m)] for i in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        print(c[i][j], end = ' ')
+    print()
