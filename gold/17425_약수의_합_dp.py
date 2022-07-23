e2a3b376b1dc847fc58af2808e7ff4add6ecc221
becoding96 <becoding96@gmail.com>
2022-07-24 01:07:16 +0900

    17425 fix

--- gold/17425_약수의_합_dp.py
+++ gold/17425_약수의_합_dp.py
@@ -2,7 +2,7 @@
 import sys
 
 x_list = []
-for _ in range(int(input())):
+for _ in range(int(sys.stdin.readline())):
     x = int(sys.stdin.readline())
     x_list.append(x)
 
@@ -17,7 +17,7 @@ for i in range(2, MAX + 1):
     while i * j <= MAX: # 에라토스테네스의 체
         eratos[i * j] += i
         j += 1
-    dp[i] = dp[i - 1] + eratos[i]
+    dp[i] = dp[i - 1] + eratos[i] # g(i)를 dp방식으로 계산
 
 for x in x_list:
     print(dp[x])
