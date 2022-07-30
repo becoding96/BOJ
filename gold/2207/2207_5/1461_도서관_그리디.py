87a9e566a667899de04e519244d9151c5abdebc9
becoding96 <becoding96@gmail.com>
2022-07-30 19:10:17 +0900

    1461 fix

--- gold/2207/2207_5/1461_도서관_그리디.py
+++ gold/2207/2207_5/1461_도서관_그리디.py
@@ -9,24 +9,20 @@ negative.sort()
 positive.sort()
 
 result = 0
-abs_max = 0
+farthest = max(max(position), -min(position))
 
 i = 0
 while i <= len(negative) - 1:
     cur = abs(negative[i])
     result += cur * 2
-    if cur > abs_max:
-        abs_max = cur
     i += m
 
 j = len(positive) - 1
 while j >= 0:
     cur = positive[j]
     result += cur * 2
-    if cur > abs_max:
-        abs_max = cur
     j -= m
 
-result -= abs_max
+result -= farthest
 
 print(result)
