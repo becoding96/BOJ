8592f4dcb9bf1124f765f85ff112a6a52cbb5601
becoding96 <becoding96@gmail.com>
2022-09-01 23:13:21 +0900

    1010

---
+++ silver/2209/2209_1/1010_다리_놓기_dp.py
@@ -0,0 +1,25 @@
+# 220901
+import sys
+
+def factorial(n):
+    result = n
+    while n >= 3:
+        n -= 1
+        result *= n
+    return result
+
+def combination(n, r):
+    if n == r:
+        return 1
+    elif r == 0 or r == 1 or n - r == 1:
+        return n
+
+    if r >= n // 2 + 1:
+        r = n - r
+    
+    return int(factorial(n) / (factorial(n - r) * factorial(r)))
+
+input = sys.stdin.readline
+for _ in range(int(input().rstrip())):
+    N, M = map(int, input().split())
+    print(combination(M, N))
