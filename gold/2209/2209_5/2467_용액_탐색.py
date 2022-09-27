050844d173ebf078117f80f4395bc1c0eab07f92
becoding96 <becoding96@gmail.com>
2022-09-27 21:36:57 +0900

    2467

---
+++ gold/2209/2209_5/2467_용액_탐색.py
@@ -0,0 +1,18 @@
+# 220927
+
+import sys
+
+input = sys.stdin.readline
+N = int(input().rstrip())
+liquid = sorted(list(map(int, input().split())), key=lambda x: abs(x))
+mix_min = 2000000000
+result = [liquid[0], liquid[1]]
+
+for i in range(N - 1):
+    mix = abs(liquid[i] + liquid[i + 1])
+    if mix < mix_min:
+        mix_min = mix
+        result[0], result[1] = liquid[i], liquid[i + 1]
+
+result.sort()
+print(*result)
