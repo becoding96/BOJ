02ace5db1e8f52b6cbd35cb00b8357331baddbf6
becoding96 <becoding96@gmail.com>
2022-10-14 11:07:39 +0900

    1277

--- gold/2210/1277_발전소_설치_다익스트라.py
+++ gold/2210/1277_발전소_설치_다익스트라.py
@@ -10,17 +10,12 @@ def dijkstra(s):
         D[i] = board[s][i]
 
     for _ in range(N - 1):
-        minV = M
+        minV = 300000
         w = 0
         for i in range(1, N + 1):
             if U[i] == 0 and 0 <= D[i] < minV:
                 minV = D[i]
                 w = i
-        if w == 0:
-            for i in range(1, N + 1):
-                if U[i] == 0:
-                    w = i
-                    break
         U[w] = 1
         for v in range(1, N + 1):
             if 0 <= board[w][v] <= M:
@@ -30,21 +25,16 @@ def dijkstra(s):
 N, W = map(int, input().split())
 M = float(input().rstrip())
 points = [()] + [tuple(map(int, input().split())) for _ in range(N)]
-board = [[M] * (N + 1) for _ in range(N + 1)]
-
-for i in range(N + 1):
-    board[i][i] = -1
+board = [[-1] * (N + 1) for _ in range(N + 1)]
 
 for i in range(1, N + 1):
     for j in range(i + 1, N + 1):
-        if i == j:
-            continue
-        board[i][j] = board[j][i] = ((points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2) ** (1/2)
+        if i != j:
+            board[i][j] = board[j][i] = ((points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2) ** (1/2)
 
 for _ in range(W):
-    x, y = map(int, input().split())
-    board[x][y] = 0
-    board[y][x] = 0
+    a, b = map(int, input().split())
+    board[a][b] = board[b][a] = 0
 
 D = [-1] * (N + 1)
 dijkstra(1)
