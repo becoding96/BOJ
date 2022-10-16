b0bfe10e97552abebe6f6340edfdc58b8de5ed39
becoding96 <becoding96@gmail.com>
2022-10-16 21:21:20 +0900

    line fix

--- ssafy/permutation, combination/permutation_SSAFY.py
+++ ssafy/permutation, combination/permutation_SSAFY.py
@@ -13,19 +13,17 @@ R = 3
 p = [i for i in range(1, N + 1)]
 perm1(0, N, R)
 
-print("###########################################################")
-
 
 def perm2(i, k, r):
     if i == r:
         print(p)
     else:
         for j in range(k):
-            if used[j] == 0:       # a[j] 가 아직 사용되지 않았으면
-                used[j] = 1        # a[j] 사용됨으로 표시
-                p[i] = a[j]        # p[i]는 a[j]로 결정
+            if used[j] == 0:        # a[j] 가 아직 사용되지 않았으면
+                used[j] = 1         # a[j] 사용됨으로 표시
+                p[i] = a[j]         # p[i]는 a[j]로 결정
                 perm2(i + 1, k, r)  # p[i + 1] 값을 결정하러 이동
-                used[j] = 0        # a[j]를 다른 자리에서 쓸 수 있도록 해제
+                used[j] = 0         # a[j]를 다른 자리에서 쓸 수 있도록 해제
 
 
 N = 5
