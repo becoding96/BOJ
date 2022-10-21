219c12cc0a198372a0933f4c58776804048477a5
becoding96 <becoding96@gmail.com>
2022-10-21 16:15:45 +0900

    1806

---
+++ boj/gold/22/10/1806_부분합_투포인터.py
@@ -0,0 +1,29 @@
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+result = N
+cumulated = 0
+
+l = r = 0
+cumulated = arr[0]
+while r < N - 1:
+    if arr[r] >= S:
+        print(1)
+        exit()
+    if cumulated >= S:
+        while cumulated - arr[l] >= S:
+            cumulated -= arr[l]
+            l += 1
+        result = r - l + 1 if r - l + 1 < result else result
+    r += 1
+    cumulated += arr[r]
+
+if cumulated >= S:
+    while cumulated - arr[l] >= S:
+        cumulated -= arr[l]
+        l += 1
+    result = r - l + 1 if r - l + 1 < result else result
+
+if sum(arr) < S:
+    print(0)
+else:
+    print(result)
