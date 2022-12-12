3c6b206f464073328e9538e9b0c270ceb0ed6393
becoding96 <becoding96@gmail.com>
2022-12-13 03:02:05 +0900

    9328

--- boj/gold/22/12/9328_열쇠.py
+++ boj/gold/22/12/9328_열쇠.py
@@ -1,15 +1,4 @@
 # 221212
-
-'''
-PyPy3 - 2080 ms
-
-입력받을 때 문의 위치들을 다 저장해놓고, 열쇠를 얻었을 때 열 수 있는 문 다 열기
-테두리에 있는 문이라면 시작 위치에 넣어주기
-
-열쇠의 개수가 변함이 없으면 갈 수 있는 위치가 같다는 뜻이므로
-bfs를 한 사이클 돌았을 때 열쇠의 개수가 변함이 없으면 끝낸다.
-'''
-
 import sys; input = sys.stdin.readline
 from collections import deque, defaultdict
 
@@ -17,34 +6,29 @@ from collections import deque, defaultdict
 def sol(i, j):
     global result
     q = deque([(i, j)])
-    visited = [[0 for _ in range(w)] for _ in range(h)]
-    visited[i][j] = 1
 
     while q:
         v = q.popleft()
-        # print(v)
         for di, dj in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
             ni, nj = v[0] + di, v[1] + dj
 
-            if ni < 0 or ni >= h or nj < 0 or nj >= w or building[ni][nj] == '*' or visited[ni][nj] or 65 <= ord(building[ni][nj]) <= 90:
-                # 다음 탐색 위치가 문이라면 볼 필요가 없다.
-                # 어짜피 열쇠를 얻을 때마다 열 수 있는 문은 다 열어놓을 것
+            if ni < 0 or ni >= h or nj < 0 or nj >= w or building[ni][nj] == '*':
                 continue
             
             char = building[ni][nj]
-            visited[ni][nj] = 1  # 방문 체크
+            building[ni][nj] = '*'  # 방문 체크
              
             if char == '$':  # 다음 탐색 위치가 문서라면
                 result += 1
-                building[ni][nj] = '.'
+            elif 65 <= ord(char) <= 90:  # 다음 탐색 위치가 문이라면
+                if not char.lower() in keys:  # 열쇠가 없으면 막힘
+                    blocked_doors[char].append((ni, nj))  # 막혔던 위치 저장
+                    continue  # q에 append되면 안됨!
             elif 97 <= ord(char) <= 122:  # 다음 탐색 위치가 열쇠라면
                 if not char in keys:
                     keys.add(char)  # 열쇠 목록에 저장
-                    for door_pos in doors[char.upper()]:  
-                        building[door_pos[0]][door_pos[1]] = '.'  # 해당 열쇠로 열 수 있는 문 다 열어놓기
-                        # 테두리 위치의 문을 열었으면 시작 포인트에 추가
-                        if door_pos[0] == 0 or door_pos[0] == h - 1 or door_pos[1] == 0 or door_pos[1] == w - 1:  
-                            starts.append((door_pos[0], door_pos[1]))
+                    for blocked_door in blocked_doors[char.upper()]:  # 막혔던 곳 다시 가보기
+                        q.append(blocked_door)
             
             q.append((ni, nj))
 
@@ -60,59 +44,45 @@ for T in range(int(input().rstrip())):
     result = 0
     
     starts = []  # 시작 위치 저장용 리스트
-    doors = defaultdict(list)  # 문 위치 저장용 딕셔너리
+    blocked_doors = defaultdict(list)  # 막혔던 위치 저장용 딕셔너리
+
     for i in range(h):
         for j in range(w):
-            # 문 위치 저장해놓기
-            if 65 <= ord(building[i][j]) <= 90:
-                if building[i][j].lower() in keys:
-                    building[i][j] = '.'
-                else:
-                    doors[building[i][j]].append((i, j))
+            char = building[i][j]
             
-            # 초기에 시작 위치 넣어주기
-            if i == 0 or i == h - 1:
-                char = building[i][j]
-                if char == '*' or 65 <= ord(char) <= 90:  # 이미 열쇠가 있는 문들은 열어놨으므로, 문이 나오면 넘어감
-                    continue
-                starts.append((i, j))
+            # 시작 위치 넣어주기
+            if i == 0 or i == h - 1:  # 첫 행과 마지막 행에서
+                building[i][j] = '*'  # 시작점 방문 체크
                 if char == '$':
                     result += 1
+                    starts.append((i, j))
+                elif 65 <= ord(char) <= 90:
+                    blocked_doors[char].append((i, j))
                 elif 97 <= ord(char) <= 122:
                     keys.add(char)
-                building[i][j] = '.'  # 문서든 열쇠든, 주웠으니까 빈 칸으로
-        
-        # 초기에 시작 위치 넣어주기
-        if i != 0 and i != h - 1:
-            for j in [0, w - 1]:
-                char = building[i][j]
-                if char == '*' or 65 <= ord(char) <= 90:
-                    continue
-                starts.append((i, j))
-                if char == '$':
-                    result += 1
-                elif 97 <= ord(char) <= 122:
-                    keys.add(char)
-                building[i][j] = '.'
-    
-    # print(doors)
-    # print(starts)
+                    starts.append((i, j))
+                elif char == '.':
+                    starts.append((i, j))
+            else:
+                if j == 0 or j == w - 1:
+                    building[i][j] = '*'
+                    if char == '$':
+                        result += 1
+                        starts.append((i, j))
+                    elif 65 <= ord(char) <= 90:
+                        blocked_doors[char].append((i, j))
+                    elif 97 <= ord(char) <= 122:
+                        keys.add(char)
+                        starts.append((i, j))
+                    elif char == '.':
+                        starts.append((i, j))
 
+    # 시작 테두리에서 주웠던 열쇠들에 맞는 문 열어주기
     for key in keys:
-        for door_pos in doors[key.upper()]:  
-            building[door_pos[0]][door_pos[1]] = '.'  # 해당 열쇠로 열 수 있는 문 다 열어놓기
-            # 테두리 위치의 문을 열었으면 시작 포인트에 추가
-            if door_pos[0] == 0 or door_pos[0] == h - 1 or door_pos[1] == 0 or door_pos[1] == w - 1:  
-                starts.append((door_pos[0], door_pos[1]))
+        for blocked_door in blocked_doors[key.upper()]:  # 테두리에서 막혔던 문 시작 위치에 넣기
+            starts.append(blocked_door)
 
-    num_of_key = len(keys)
-    while True:
-        for start in starts:
-            sol(start[0], start[1])
-
-        if num_of_key == len(keys):  
-            break
-        else:
-            num_of_key = len(keys)
+    for start in starts:
+        sol(start[0], start[1])
 
     print(result)
