49e39e3f74df681da337428282f5ca4b77fc14d5
becoding96 <becoding96@gmail.com>
2023-04-16 21:29:47 +0900

    2002

--- boj/silver/23/04/2002_추월.py
+++ boj/silver/23/04/2002_추월.py
@@ -10,8 +10,10 @@ cnt = 0
 for _ in range(N):
     in_number = t_in[input().rstrip()]
     before_sum = sum(t_out[:in_number])
+
     if before_sum < in_number:
         cnt += 1
+        
     t_out[in_number] = 1
 
 print(cnt)
