fe03cd28709c89e1c5df1ec39ccd08cf5614489f
becoding96 <becoding96@gmail.com>
2023-05-28 14:48:19 +0900

    프로그래머스 150369 주석

--- programmers/23/5/150369.py
+++ programmers/23/5/150369.py
@@ -1,26 +1,28 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    d_idx, p_idx = n - 1, n - 1
+    d_idx, p_idx = n - 1, n - 1  # 뒤에서 출발
 
     while d_idx >= 0 or p_idx >= 0:
         d_cap, p_cap = cap, cap
         max_length = 0
 
+        # 배달
         for i in range(d_idx, -1, -1):
             if d_cap == 0:
                 break
 
             if deliveries[i]:
-                if deliveries[i] >= d_cap:
+                if deliveries[i] >= d_cap:  # cap보다 배달할 상자가 많거나 같은 경우
                     deliveries[i] -= d_cap
                     d_cap = 0
                 else:
                     d_cap -= deliveries[i]
                     deliveries[i] = 0
 
+                # 뒤에서 부터 봤을 때 최초로 배달하거나 픽업한 곳
                 max_length = max(max_length, i + 1)
 
-                d_idx = i
+                d_idx = i  # deliveries[i]가 남았을 수도 있으니 다음 반복문을 위해 i로 저장
 
         for i in range(p_idx, -1, -1):
             if p_cap == 0:
@@ -34,6 +36,7 @@ def solution(cap, n, deliveries, pickups):
                     p_cap -= pickups[i]
                     pickups[i] = 0
 
+                # 뒤에서 부터 봤을 때 최초로 배달하거나 픽업한 곳
                 max_length = max(max_length, i + 1)
 
                 p_idx = i
