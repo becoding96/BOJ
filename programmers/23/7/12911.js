95bb5ec24f2e29c1e913bcff5fd2c689499e3cc8
becoding96 <becoding96@gmail.com>
2023-07-04 04:36:25 +0900

    12911

---
+++ programmers/23/7/12911.js
@@ -0,0 +1,19 @@
+function countOne(n) {
+  return n.toString(2).split("1").length - 1;
+}
+
+function solution(n) {
+  const oneCount = countOne(n);
+
+  let next = n;
+  let i = 1;
+
+  while (true) {
+    if (countOne(next + i) === oneCount) {
+      return next + i;
+    }
+    i += 1;
+  }
+}
+
+console.log(solution(78));
