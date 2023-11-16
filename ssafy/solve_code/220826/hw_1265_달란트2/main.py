cdd67943717f26a19f72220c1f32dad8eaabd2b7
becoding96 <becoding96@gmail.com>
2023-11-16 21:22:51 +0900

    ssafy 폴더 제거

--- ssafy/solve_code/220826/hw_1265_달란트2/main.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.stdin = open("input.txt")
-
-for T in range(int(input())):
-    N, P = map(int, input().split())
-    q, r = N // P, N % P
-    print(f'#{T + 1} {q ** (P - r) * (q + 1) ** r}')
